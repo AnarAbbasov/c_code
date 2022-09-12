41fad004f2d259023ac2b19e7adefca1194fc0f2
Anar Abbasov <anar_abbasov@hotmail.com>
2022-09-11 21:09:26 -0700

    fist commit

---
+++ anar_functions.h
@@ -0,0 +1,12 @@
+
+char *  libfunc()
+    {
+	    return "siktir borya";
+     }
+
+int * retr()
+    {
+	    int myr=3652;
+     return  &myr;
+
+     }
---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include "anar_functions.h"
+
+
+
+int main ()
+     {
+     printf("Hello");
+     printf(libfunc());
+     int a=3344;
+     printf("%p\n",&a);
+     printf("%s\n","boris xuylo");
+     return 0;
+     }
